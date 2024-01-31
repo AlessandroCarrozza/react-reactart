44cfd92c1ecd3ab3880e0f88e938c156fbda6869
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2024-01-31 16:41:03 +0100

    new style comps files created

---
+++ src/components/Button.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  width: 100%;
+  padding: 0.75rem 1rem;
+  line-height: 1.5;
+  background-color: #f0b322;
+  color: #fff;
+  border: none;
+  border-radius: 0.25rem;
+  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+
+  &:hover {
+    background-color: #f0920e;
+  }
+`;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,6 +9,40 @@ const StyledHeader = styled.header`
   justify-content: center;
   margin-top: 2rem;
   margin-bottom: 2rem;
+
+  & img {
+    object-fit: contain;
+    margin-bottom: 2rem;
+    width: 11rem;
+    height: 11rem;
+  }
+
+  & h1 {
+    font-size: 1.5rem;
+    font-weight: 600;
+    letter-spacing: 0.4em;
+    text-align: center;
+    text-transform: uppercase;
+    color: #9a3412;
+    font-family: "Pacifico", cursive;
+    margin: 0;
+  }
+
+  & p {
+    text-align: center;
+    color: #a39191;
+    margin: 0;
+  }
+
+  @media (min-width: 768px) {
+    & {
+      margin-bottom: 4rem;
+    }
+
+    & h1 {
+      font-size: 2.25rem;
+    }
+  }
 `;
 
 export default function Header() {
@@ -16,7 +50,9 @@ export default function Header() {
     <StyledHeader>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p className={classes.paragraph}>
+      <p
+      // className={classes.paragraph}
+      >
         A community of artists and art-lovers.
       </p>
     </StyledHeader>
